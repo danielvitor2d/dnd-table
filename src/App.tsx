77607b581ac8700996b6f26d58c26f568297a292
Daniel Vitor Pereira Rodrigues <danieldev.ti@gmail.com>
2024-04-05 21:34:37 -0300

    new: finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,149 @@
+import { useMemo, useState } from 'react'
+import { faker } from '@faker-js/faker'
+import Table, { Column } from './components/table'
+
+type Person = {
+  name: string
+  age: number
+}
+
+function makeData(quantity: number): Person[] {
+  const data: Person[] = []
+
+  for (let i = 0; i < quantity; i++) {
+    data.push({
+      name: faker.person.firstName(),
+      age: faker.number.int({ min: 15, max: 80 }),
+    })
+  }
+
+  return data
+}
+
 export default function App() {
+  const [data] = useState(makeData(55))
+  const [pageSize, setPageSize] = useState(5)
+  const [page, setPage] = useState(0)
+
+  const options = [
+    { id: 'idx', text: 'Índice' },
+    { id: 'firstName', text: 'Primeiro Nome' },
+    { id: 'age', text: 'Idade' },
+    { id: 'visits', text: 'Visitas' },
+    { id: 'status', text: 'Status' },
+    { id: 'progress', text: 'Progresso' },
+  ]
+
+  const [selectedOptions, setSelectedOptions] = useState<string[]>(
+    options.map((p) => p.id),
+  )
+
+  const columns: Column<Person>[] = useMemo(() => {
+    const data = [
+      {
+        accessorFn: (_, idx) => idx + 1,
+        cell: (info) => info.getValue(),
+        header: 'Índice',
+        id: 'idx',
+        size: 150,
+      },
+      {
+        accessorKey: 'name',
+        cell: (info) => info.getValue(),
+        header: () => <span>Primeiro Nome</span>,
+        id: 'firstName',
+        size: 150,
+      },
+      {
+        accessorFn: (row) => row.name,
+        cell: (info) => info.getValue(),
+        header: () => <span>Último Nome</span>,
+        id: 'lastName',
+        size: 150,
+      },
+      {
+        accessorKey: 'age',
+        header: () => 'Idade',
+        id: 'age',
+        size: 120,
+      },
+      {
+        accessorFn: (info) => info.name,
+        header: () => <span>Visitas</span>,
+        id: 'visits',
+        size: 120,
+      },
+      {
+        accessorFn: (info) => info.name,
+        header: 'Status',
+        id: 'status',
+        size: 150,
+      },
+      {
+        accessorFn: (info) => info.name,
+        header: 'Progresso',
+        id: 'progress',
+        size: 180,
+      },
+    ] satisfies Column<Person>[]
+
+    return data.filter((item) =>
+      selectedOptions.find((option) => option === item.id),
+    )
+  }, [selectedOptions])
+
+  const [columnOrder, setColumnOrder] = useState<string[]>(() =>
+    columns.map((c) => c.id!),
+  )
+
+  async function prevPage() {
+    setPage((prev) => prev - 1)
+  }
+
+  async function nextPage() {
+    setPage((prev) => prev + 1)
+  }
+
+  const handleOptionChange = (id: string) => {
+    if (selectedOptions.includes(id)) {
+      setSelectedOptions(selectedOptions.filter((optionId) => optionId !== id))
+    } else {
+      setSelectedOptions([...selectedOptions, id])
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="pt-10 w-full flex flex-col gap-10 items-center">
+      <h1 className="font-bold text-3xl">Tabela com colunas reorganizáveis</h1>
+
+      <div className="flex flex-col gap-3">
+        {options.map((option) => (
+          <div key={option.id} className="flex items-center border-b">
+            <input
+              type="checkbox"
+              id={String(option.id)}
+              value={option.id}
+              checked={selectedOptions.includes(option.id)}
+              onChange={() => handleOptionChange(option.id)}
+              className="mr-2"
+            />
+            <label htmlFor={String(option.id)}>{option.text}</label>
+          </div>
+        ))}
+      </div>
+
+      <Table
+        columnOrder={columnOrder}
+        setColumnOrder={setColumnOrder}
+        columns={columns}
+        data={data}
+        page={page}
+        setPage={setPage}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
     </div>
   )
 }
---
+++ src/components/table/index.tsx
@@ -0,0 +1,314 @@
+import { CSSProperties, useMemo, useState } from 'react'
+import {
+  Cell,
+  ColumnDef,
+  Header,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import {
+  DndContext,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  closestCenter,
+  type DragEndEvent,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import { restrictToHorizontalAxis } from '@dnd-kit/modifiers'
+import {
+  arrayMove,
+  SortableContext,
+  horizontalListSortingStrategy,
+  useSortable,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+
+function LeftIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      className="w-6 h-6 stroke-zinc-500 hover:stroke-zinc-500/80"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 19.5 8.25 12l7.5-7.5"
+      />
+    </svg>
+  )
+}
+
+function RightIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      className="w-6 h-6 stroke-zinc-500 hover:stroke-zinc-500/80"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m8.25 4.5 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  )
+}
+
+export type Column<T> = ColumnDef<T>
+
+interface TableProps<T> {
+  data: Array<T>
+  columns: Array<Column<T>>
+  columnOrder: string[]
+  setColumnOrder: React.Dispatch<React.SetStateAction<string[]>>
+
+  page: number
+  setPage: React.Dispatch<React.SetStateAction<number>>
+  pageSize: number
+  setPageSize: React.Dispatch<React.SetStateAction<number>>
+  prevPage: () => Promise<void>
+  nextPage: () => Promise<void>
+}
+
+function DraggableTableHeader<T>({ header }: { header: Header<T, unknown> }) {
+  const { attributes, isDragging, listeners, setNodeRef, transform } =
+    useSortable({
+      id: header.column.id,
+    })
+
+  const style: CSSProperties = {
+    opacity: isDragging ? 0.8 : 1,
+    position: 'relative',
+    transform: CSS.Translate.toString(transform),
+    transition: 'width transform 0.2s ease-in-out',
+    whiteSpace: 'nowrap',
+    width: header.column.getSize(),
+    zIndex: isDragging ? 1 : 0,
+  }
+
+  return (
+    <th
+      colSpan={header.colSpan}
+      ref={setNodeRef}
+      style={style}
+      className="px-6 py-3"
+    >
+      <button {...attributes} {...listeners}>
+        {header.isPlaceholder
+          ? null
+          : flexRender(header.column.columnDef.header, header.getContext())}
+      </button>
+    </th>
+  )
+}
+
+function DragAlongCell<T>({ cell }: { cell: Cell<T, unknown> }) {
+  const { isDragging, setNodeRef, transform } = useSortable({
+    id: cell.column.id,
+  })
+
+  const style: CSSProperties = {
+    opacity: isDragging ? 0.8 : 1,
+    position: 'relative',
+    transform: CSS.Translate.toString(transform),
+    transition: 'width transform 0.2s ease-in-out',
+    width: cell.column.getSize(),
+    zIndex: isDragging ? 1 : 0,
+  }
+
+  return (
+    <td style={style} ref={setNodeRef} className="px-6 py-4">
+      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+    </td>
+  )
+}
+
+export default function Table<T>({
+  data,
+  columns,
+  columnOrder,
+  setColumnOrder,
+  page,
+  setPage,
+  pageSize,
+  setPageSize,
+  nextPage,
+  prevPage,
+}: TableProps<T>) {
+  const [curPage, setCurPage] = useState(1)
+  const total = useMemo(() => data.length, [data])
+  const fromItem = useMemo(() => page * pageSize, [page, pageSize])
+  const toItem = useMemo(
+    () => Math.min(total - 1, page * pageSize + pageSize - 1),
+    [page, pageSize, total],
+  )
+  const lastPage = useMemo(() => Math.ceil(total / pageSize), [total, pageSize])
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    pageCount: total / pageSize + (total % pageSize === 0 ? 0 : 1),
+    state: {
+      columnOrder,
+    },
+    manualPagination: true,
+    onColumnOrderChange: setColumnOrder,
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: true,
+  })
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+    if (active && over && active.id !== over.id) {
+      setColumnOrder((columnOrder) => {
+        const oldIndex = columnOrder.indexOf(active.id as string)
+        const newIndex = columnOrder.indexOf(over.id as string)
+        return arrayMove(columnOrder, oldIndex, newIndex)
+      })
+    }
+  }
+
+  function handlePrevPage() {
+    page > 0 && prevPage()
+  }
+
+  function handleNextPage() {
+    page < lastPage - 1 && nextPage()
+  }
+
+  function handlePageSizeChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    setPageSize(parseInt(e.target.value))
+    setPage(0)
+    setCurPage(0)
+  }
+
+  function handleChangePage() {
+    if (curPage > 0 && curPage <= lastPage) {
+      setPage(curPage - 1)
+    }
+  }
+
+  const sensors = useSensors(
+    useSensor(MouseSensor, {}),
+    useSensor(TouchSensor, {}),
+    useSensor(KeyboardSensor, {}),
+  )
+
+  return (
+    <DndContext
+      collisionDetection={closestCenter}
+      modifiers={[restrictToHorizontalAxis]}
+      onDragEnd={handleDragEnd}
+      sensors={sensors}
+    >
+      <div className="w-full p-2">
+        <div className="w-full h-4" />
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                <SortableContext
+                  items={columnOrder}
+                  strategy={horizontalListSortingStrategy}
+                >
+                  {headerGroup.headers.map((header) => (
+                    <DraggableTableHeader key={header.id} header={header} />
+                  ))}
+                </SortableContext>
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table
+              .getRowModel()
+              .rows.slice(fromItem, toItem + 1)
+              .map((row) => (
+                <tr key={row.id} className="bg-white border-b">
+                  {row.getVisibleCells().map((cell) => (
+                    <SortableContext
+                      key={cell.id}
+                      items={columnOrder}
+                      strategy={horizontalListSortingStrategy}
+                    >
+                      <DragAlongCell key={cell.id} cell={cell} />
+                    </SortableContext>
+                  ))}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <div className="py-2 px-4 border-b text-xs md:text-sm text-gray-700 flex flex-row justify-between items-center">
+          <p className="">
+            Mostrando {1 + fromItem} até {1 + toItem} de {total} items
+          </p>
+
+          <div className="flex flex-row items-center gap-4 text-gray-500">
+            <div className="w-fit flex flex-row gap-0 items-center text-center rounded-md border-zinc-300 border-y-[1px] *:border-zinc-300 *:border-l-[1px] *:border-r-[1px] cursor-pointer">
+              <button
+                type="button"
+                disabled={page === 0}
+                onClick={() => handlePrevPage()}
+                className="w-10 h-8 flex items-center justify-center p-1 bg-white hover:bg-zinc-200 rounded-s-md"
+              >
+                <LeftIcon />
+              </button>
+              <div className="w-10 h-8 flex items-center justify-center border-none hover:bg-blue-500 hover:text-zinc-50">
+                <p>{page + 1}</p>
+              </div>
+              <button
+                type="button"
+                disabled={page === lastPage - 1}
+                onClick={() => handleNextPage()}
+                className="w-10 h-8 flex items-center justify-center p-1 bg-white hover:bg-zinc-200 rounded-e-md"
+              >
+                <RightIcon />
+              </button>
+            </div>
+
+            <div className="flex flex-row items-center gap-2">
+              <input
+                type="number"
+                value={curPage}
+                min={1}
+                className="w-16 h-8 p-1 border-zinc-300 border-[1px] rounded-md text-center ring-0 outline-none focus:border-[2px] focus:border-blue-500"
+                onChange={(e) => setCurPage(Number(e.target.value))}
+              />
+              <button
+                type="button"
+                onClick={handleChangePage}
+                className="w-10 h-8 p-1 border-zinc-300 bg-blue-500 hover:bg-blue-600 border-[1px] rounded-md text-center text-white"
+              >
+                Ir
+              </button>
+            </div>
+
+            <div className="w-fit flex flex-row items-center gap-2">
+              <p>Por página</p>
+              <select
+                value={pageSize}
+                onChange={handlePageSizeChange}
+                className="block appearance-none w-20 bg-white text-zinc-500 py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none border-zinc-300 border-[1px] focus:border-[2px] focus:border-blue-500"
+              >
+                <option value={5}>5</option>
+                <option value={10}>10</option>
+                <option value={15}>15</option>
+                <option value={20}>20</option>
+                <option value={50}>50</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DndContext>
+  )
+}
